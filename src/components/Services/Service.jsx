9d7f0785ddcb4d36fbfa00ba96e75ec49070322a
Cristian Windows Predator <xthecapx@gmail.com>
2020-02-13 17:06:11 -0500

    1. Added initial footer 2. Added initial Services

--- src/components/Services/Service.jsx
+++ src/components/Services/Service.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import Grid from '@material-ui/core/Grid';
 
-const Service = () => {
-  return <div></div>;
+const Service = ({ title, description, button, icon }) => {
+  return (
+    <div className="kos__card column">
+      <Grid container>
+        <Grid item>{icon}</Grid>
+        <Grid item xs={11}>
+          <h3>{title}</h3>
+          <p>{description}</p>
+          <button>{button}</button>
+        </Grid>
+      </Grid>
+    </div>
+  );
 };
 
 export default Service;
--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,8 +1,24 @@
-
 import React from 'react';
+import Grid from '@material-ui/core/Grid';
 
-const Services = () => {
-  return <div></div>;
+import Service from './Service';
+
+const Services = ({ services }) => {
+  console.log(services);
+  return (
+    <div className="kos__services container">
+      <div className="columns">
+        {services.map(service => (
+          <Service
+            title={service.title}
+            description={service.description}
+            button={service.button}
+            icon='icon'
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Services;
