e7ff98c3e083e92591edc70fc150fd0e490f646b
Cristian Windows Predator <xthecapx@gmail.com>
2020-02-13 15:44:31 -0500

    1. Clean About Page to start working on it

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -1,27 +1,13 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { graphql } from 'gatsby';
 import Layout from '../components/Layouts/Main';
-import Content, { HTMLContent } from '../components/Content';
 
 export const AboutPageTemplate = ({ title, content, contentComponent }) => {
-  const PageContent = contentComponent || Content;
-
   return (
-    <section className="section section--gradient">
-      <div className="container">
-        <div className="columns">
-          <div className="column is-10 is-offset-1">
-            <div className="section">
-              <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
-                {title}
-              </h2>
-              <PageContent className="content" content={content} />
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
+    <Fragment>
+      <div>{title}</div>
+    </Fragment>
   );
 };
 
@@ -32,15 +18,15 @@ AboutPageTemplate.propTypes = {
 };
 
 const AboutPage = ({ data }) => {
-  const { markdownRemark: post } = data;
+  const { frontmatter } = data.markdownRemark;
 
   return (
-    <Layout navbarFixed={true}>
-      <AboutPageTemplate
-        contentComponent={HTMLContent}
-        title={post.frontmatter.title}
-        content={post.html}
-      />
+    <Layout
+      Header={() => {
+        return <div className="container">container two</div>;
+      }}
+    >
+      <AboutPageTemplate {...frontmatter} />
     </Layout>
   );
 };
